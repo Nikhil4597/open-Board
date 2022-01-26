53079a230a9ac428e2733a5c8b72761e58727ad5
Nikhil4597 <bramanrohit@gmail.com>
2022-01-27 01:50:27 +0530

    push again

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
     let canvasBoard = document.querySelector("canvas");
     canvasBoard.height = window.innerHeight;
     canvasBoard.width = window.innerWidth;
-    let tool = canvasBoard.getContext("2d");
     //using tools
     // let rectTool = document.querySelector(".fa-square");
     // let lineTool = document.querySelector(".fa-grip-lines-vertical");
+    
+    let tool = canvasBoard.getContext("2d");
     let cTool = "rectTool";
+
     // //for boundary
-    // tool.strokeStyle ="red"
+    tool.strokeStyle ="grey";
     // tool.strokeRect(50,50,200,200);
 
     // for filling area  change
     // tool.fillRect(50,50,100,100);
-    // // tool.fillStyle ="green";
-    $(".fa-square").click(function(){
+    // tool.fillStyle = "green";
+   
+    $(".rect").click(function(){
+
         cTool = "rectTool";
     });
 
-    $(".fa-grip-lines-vertical").click(function(){
+    $(".line").click(function(){
         cTool = "lineTool";
     });
 
@@ -26,32 +30,130 @@
     let delX = border.left; //dis between body and canvas in x & y
     let delY = border.top;
     let isPen = false;
+    console.log($(".toolbar"));
+    let isBox = false;
+    $("#pencil").click(function(){
+    if(!isBox){
+        $(this).html(`
+        <img src="./NewIcons/Black_Pencil.svg" alt="" class="tool-img">
 
-    $("body").mouseup(function(e){
-        isPen = false;
-    });
-
-
-    $("body").mousedown(function(e){
-        isPen = true;
-        tool.beginPath();
-        ix = e.clientX-delX;
-        iy = e.clientY-delY;
-        tool.moveTo(ix,iy);
-    });
-    
-    $("body").mousemove(function(e){
-        if(isPen ==false)
-        {
-            return ;
+        <div class="size-box">
+        <div class="size1 size"></div>
+        <div class="size2 size"></div>
+        <div class="size3 size"></div>
+        <div class="size4 size"></div>
+    </div>`);
+        isBox =true;
+        
         }
-        fx = e.clientX-delX;
-        fy = e.clientY-delY;
-        tool.lineTo(fx,fy);
-        tool.stroke();
-        ix = fx;
-        iy = fy;
-    })
+        else{
+            $(this).html(`
+            <img src="./NewIcons/Black_Pencil.svg" alt="" class="tool-img">
+     `);
+            
+            isBox = false;
+        }
+    });
+
+    $(".eraser").click(function(){
+        isBox = false;
+    if(!isBox){
+        $(this).html(`
+        <img src="./NewIcons/Eraser.svg" alt="" class="tool-img">
+
+        <div class="size-box">
+        <div class="size1 size"></div>
+        <div class="size2 size"></div>
+        <div class="size3 size"></div>
+        <div class="size4 size"></div>
+    </div>`);
+        isBox =true;
+        
+        }
+        else{
+            $(this).html(`
+            <img src="./NewIcons/Eraser.svg" alt="" class="tool-img">
+     `);
+            
+            isBox = false;
+        }});
+
+    $(".rect").click(function(){
+        isBox = false;
+        if(!isBox){
+            $(this).html(`
+            <img src="./NewIcons/simple-green-rectangle.svg" alt="" class="tool-img">
+
+            <div class="size-box">
+            <div class="size1 size"></div>
+            <div class="size2 size"></div>
+            <div class="size3 size"></div>
+            <div class="size4 size"></div>
+        </div>`);
+            isBox =true;
+            
+            }
+            else{
+                $(this).html(`
+            <img src="./NewIcons/simple-green-rectangle.svg" alt="" class="tool-img">
+ `);
+                
+                isBox = false;
+            }
+    }); 
+    $(".line").click(function(){
+        isBox = false;
+        if(!isBox){
+        $(this).html(`
+        <img src="./NewIcons/line.svg" alt=""class="tool-img">
+
+        <div class="size-box">
+        <div class="size1 size"></div>
+        <div class="size2 size"></div>
+        <div class="size3 size"></div>
+        <div class="size4 size"></div>
+    </div>`);
+        isBox =true;
+        
+        }
+        else{
+            $(this).html(`
+            <img src="./NewIcons/line.svg" alt=""class="tool-img">    
+            `);
+            
+            isBox = false;
+        }
+    });
+    $("#pencil").click(function(){
+         let pix;
+         let piy;
+         let pfx;
+         let pfy;
+        $("body").mouseup(function(e){
+            isPen = false;
+        });
+
+        $("body").mousedown(function(e){
+            isPen = true;
+            tool.beginPath();
+            pix = e.clientX-delX;
+            piy = e.clientY-delY;
+            tool.moveTo(pix,piy);
+        });
+        
+        $("body").mousemove(function(e){
+            if(isPen == false)
+            {
+                return ;
+            }
+            pfx = e.clientX-delX;
+            pfy = e.clientY-delY;
+            tool.lineTo(pfx,pfy);
+            tool.stroke();
+            tix = pfx;
+            tiy = pfy;
+        })
+    });
     // $("body").mousedown(function(e){
     //     ix = e.clientX;
     //     iy = e.clientY;
@@ -77,13 +179,7 @@
 
     //         }
     // });
-    // color change
-    $(".red").click(function(){
-        tool.strokeStyle ="red";
-    });
-    $(".green").click(function(){
-        tool.strokeStyle ="green";
-    });
-    $(".blue").click(function(){
-        tool.strokeStyle = "blue";
-    });
+
+    // --------------------------color change
+     
+  
