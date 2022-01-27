9d1bd8d9d88fdaedb17e1d99a8ad04ffe06cc5f7
Nikhil4597 <bramanrohit@gmail.com>
2022-01-27 23:39:35 +0530

    add code

--- script.js
+++ script.js
@@ -6,180 +6,173 @@
     // let lineTool = document.querySelector(".fa-grip-lines-vertical");
     
     let tool = canvasBoard.getContext("2d");
-    let cTool = "rectTool";
+    let cTool = "pencil";
 
     // //for boundary
-    tool.strokeStyle ="grey";
+    tool.strokeStyle ="#343434";
     // tool.strokeRect(50,50,200,200);
 
     // for filling area  change
     // tool.fillRect(50,50,100,100);
     // tool.fillStyle = "green";
-   
+    let box = $(".size-box");
+    $("#pencil").click(function(){
+        if(cTool =='pencil')
+        {
+            box[0].style.display ="flex";
+        }
+        else{
+             removeBox();
+
+            cTool="pencil";
+        }
+    });
+    
+    $(".eraser").click(function(){
+        if(cTool =='erase')
+        {
+            box[1].style.display ="flex";
+        }
+        else{
+             removeBox();
+            cTool="erase";
+        }
+    });
     $(".rect").click(function(){
 
-        cTool = "rectTool";
+        if(cTool =='rect')
+        {
+            box[2].style.display ="flex";
+        }
+        else{
+            removeBox();
+            cTool="rect";
+        }
     });
 
     $(".line").click(function(){
-        cTool = "lineTool";
+        if(cTool =='line')
+        {
+            box[3].style.display ="flex";
+        }
+        else{
+              removeBox();
+            cTool="line";
+        }
     });
-
+    function removeBox(){
+        for(let i =0;i<4;i++)
+             {
+                box[i].style.display ="none";
+             }
+    }
     let ix,iy,fx,fy; 
     let border = canvasBoard.getBoundingClientRect();
     let delX = border.left; //dis between body and canvas in x & y
     let delY = border.top;
     let isPen = false;
     console.log($(".toolbar"));
-    let isBox = false;
-    $("#pencil").click(function(){
-    if(!isBox){
-        $(this).html(`
-        <img src="./NewIcons/Black_Pencil.svg" alt="" class="tool-img">
+    // $("#pencil").click(function(){
+    //      let pix;
+    //      let piy;
+    //      let pfx;
+    //      let pfy;
+    //     $("body").mouseup(function(e){
+    //         isPen = false;
+    //     });
 
-        <div class="size-box">
-        <div class="size1 size"></div>
-        <div class="size2 size"></div>
-        <div class="size3 size"></div>
-        <div class="size4 size"></div>
-    </div>`);
-        isBox =true;
+    //     $("body").mousedown(function(e){
+    //         isPen = true;
+    //         tool.beginPath();
+    //         pix = e.clientX-delX;
+    //         piy = e.clientY-delY;
+    //         tool.moveTo(pix,piy);
+    //     });
         
-        }
-        else{
-            $(this).html(`
-            <img src="./NewIcons/Black_Pencil.svg" alt="" class="tool-img">
-     `);
-            
-            isBox = false;
-        }
-    });
+    //     $("body").mousemove(function(e){
+    //         if(isPen == false)
+    //         {
+    //             return ;
+    //         }
+    //         pfx = e.clientX-delX;
+    //         pfy = e.clientY-delY;
+    //         tool.lineTo(pfx,pfy);
+    //         tool.stroke();
+    //         tix = pfx;
+    //         tiy = pfy;
+    //     })
+    // });
 
-    $(".eraser").click(function(){
-        isBox = false;
-    if(!isBox){
-        $(this).html(`
-        <img src="./NewIcons/Eraser.svg" alt="" class="tool-img">
 
-        <div class="size-box">
-        <div class="size1 size"></div>
-        <div class="size2 size"></div>
-        <div class="size3 size"></div>
-        <div class="size4 size"></div>
-    </div>`);
-        isBox =true;
-        
-        }
-        else{
-            $(this).html(`
-            <img src="./NewIcons/Eraser.svg" alt="" class="tool-img">
-     `);
-            
-            isBox = false;
-        }});
-
-    $(".rect").click(function(){
-        isBox = false;
-        if(!isBox){
-            $(this).html(`
-            <img src="./NewIcons/simple-green-rectangle.svg" alt="" class="tool-img">
-
-            <div class="size-box">
-            <div class="size1 size"></div>
-            <div class="size2 size"></div>
-            <div class="size3 size"></div>
-            <div class="size4 size"></div>
-        </div>`);
-            isBox =true;
-            
-            }
-            else{
-                $(this).html(`
-            <img src="./NewIcons/simple-green-rectangle.svg" alt="" class="tool-img">
- `);
-                
-                isBox = false;
-            }
-    }); 
-    $(".line").click(function(){
-        isBox = false;
-        if(!isBox){
-        $(this).html(`
-        <img src="./NewIcons/line.svg" alt=""class="tool-img">
-
-        <div class="size-box">
-        <div class="size1 size"></div>
-        <div class="size2 size"></div>
-        <div class="size3 size"></div>
-        <div class="size4 size"></div>
-    </div>`);
-        isBox =true;
-        
-        }
-        else{
-            $(this).html(`
-            <img src="./NewIcons/line.svg" alt=""class="tool-img">    
-            `);
-            
-            isBox = false;
-        }
-    });
-    $("#pencil").click(function(){
-         let pix;
-         let piy;
-         let pfx;
-         let pfy;
-        $("body").mouseup(function(e){
-            isPen = false;
-        });
-
-        $("body").mousedown(function(e){
+    $("body").mousedown(function(e){
+        ix = e.clientX-delX;
+        iy = e.clientY-delY;
+        if(cTool == "pencil")
+        {
             isPen = true;
             tool.beginPath();
-            pix = e.clientX-delX;
-            piy = e.clientY-delY;
-            tool.moveTo(pix,piy);
-        });
-        
-        $("body").mousemove(function(e){
-            if(isPen == false)
-            {
-                return ;
-            }
-            pfx = e.clientX-delX;
-            pfy = e.clientY-delY;
-            tool.lineTo(pfx,pfy);
-            tool.stroke();
-            tix = pfx;
-            tiy = pfy;
-        })
+            tool.moveTo(ix,iy);
+        }
     });
-    // $("body").mousedown(function(e){
-    //     ix = e.clientX;
-    //     iy = e.clientY;
-    // });
-    // $("body").mouseup(function(e){
-    //         fx = e.clientX;
-    //         fy = e.clientY;
+    $("body").mouseup(function(e){
+        if(cTool =="pencil")
+        {
+            isPen = false;
+        }
+        else if(cTool  == "line" ||  cTool == "rect"){
+            fx = e.clientX-delX;
+            fy = e.clientY-delY;
 
-    //         let width = fx-ix;
-    //         let height = fy-iy;
+            let width = fx-ix;
+            let height = fy-iy;
+            // correctness  
+             
+            
+            if(cTool == "rect"){
+                tool.strokeRect(ix,iy,width,height);  
+            }
+            else if(cTool ="line"){
+                 
+                tool.beginPath();
+                tool.moveTo(ix,iy);
+                tool.lineTo(fx,fy);
+                tool.stroke();
+            }
+        }
+    });
+    $("body").mousemove(function(e){
+        
+        if(isPen ==false)
+        {
+            return;
 
-    //         // correctness  
-    //         ix-=delX;iy-=delY;
-    //         fx-=delX;fy-=delY;
-    //         if(cTool == "rectTool"){
-    //             tool.strokeRect(ix,iy,width,height);  
-    //         }
-    //         else{
-    //             tool.beginPath();
-    //             tool.moveTo(ix,iy);
-    //             tool.lineTo(fx,fy);
-    //             tool.stroke();
-
-    //         }
-    // });
+        }
+        else{
+            fx = e.clientX-delX;
+            fy = e.clientY-delY;
+            tool.lineTo(fx,fy);
+            tool.stroke();
+            ix = fx;
+            iy = fy;
+        }
+    });
 
     // --------------------------color change
+    $(".red").click(function(){
+        tool.strokeStyle = "#ff6347";
+    });
+
+    $(".blue").click(function(){
+        tool.strokeStyle = "#4682b4";
+    });
+
+    $(".green").click(function(){
+        tool.strokeStyle = "#32cd32";
+    });
+    $(".black").click(function(){
+        tool.strokeStyle = "#343434";
+    })
+    
+
      
   
